860565354ae5dfbe266321f3f6e3d07858a5088a
martin.paces <martin.paces@eox.at>
2014-07-01 19:25:39 +0200

    Centroid/GML-Point factory + style refinement.

--- metadata/ns_gml32.py
+++ metadata/ns_gml32.py
@@ -1,8 +1,8 @@
 #------------------------------------------------------------------------------
-# 
+#
 #   GML v3.2 namespace
 #
-# Project: XML Metadata Handling 
+# Project: XML Metadata Handling
 # Authors: Martin Paces <martin.paces@eox.at>
 #
 #-------------------------------------------------------------------------------
@@ -11,8 +11,8 @@
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all
@@ -27,199 +27,177 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
-# swapped EPSG codes 
-from epsg_swapped_axes import EPSG_AXES_REVERSED 
+# swapped EPSG codes
+from epsg_swapped_axes import EPSG_AXES_REVERSED
 
 from lxml.builder import ElementMaker
-from xml_utils import nn 
-import uuid 
+from xml_utils import nn
+import uuid
 
 #------------------------------------------------------------------------------
-# get random GML identifier 
-def getRandomId() : return { ID : "uuid_%s"%( uuid.uuid4() ) }  
+# get random GML identifier
+def getRandomId(): return {ID: "uuid_%s"%(uuid.uuid4())}
 
-def getId(id=None) : 
-    if id is None :
-        return getRandomId() 
-    else : 
-        return { ID : str(id) } 
-
-#------------------------------------------------------------------------------
-# namespace 
-
-NS="http://www.opengis.net/gml/3.2"
-NS_MAP={ "gml" : NS }
+def getId(identifier=None):
+    if identifier is None:
+        return getRandomId()
+    else:
+        return {ID: str(identifier)}
 
 #------------------------------------------------------------------------------
-# element maker 
+# namespace
 
-E = ElementMaker(namespace=NS,nsmap=NS_MAP) 
-
-#------------------------------------------------------------------------------
-# predefined fully qualified names 
-
-# attributes 
-id=nn(NS,"id") ; ID=id 
-srsName=nn(None,"srsName")
-srsName=nn(None,"srsDimension")
-axisLabels=nn(None,"axisLabels")
-uomLabels=nn(None,"uomLabels")
-
-# elements 
-TimeInstant     = nn(NS,"TimeInstant") 
-TimePeriod      = nn(NS,"TimePeriod") 
-MultiSurface    = nn(NS,"MultiSurface") 
-Polygon         = nn(NS,"Polygon") 
-LinearRing      = nn(NS,"LinearRing") 
-
-beginPosition   = nn(NS,"beginPosition") 
-endPosition     = nn(NS,"endPosition") 
-timePosition    = nn(NS,"timePosition") 
-surfaceMember   = nn(NS,"surfaceMember") 
-exterior        = nn(NS,"exterior") 
-interior        = nn(NS,"interior") 
-posList         = nn(NS,"posList") 
-
-#X=nn(NS,"X") 
+NS = "http://www.opengis.net/gml/3.2"
+NS_MAP = {"gml": NS}
 
 #------------------------------------------------------------------------------
-# element factories 
+# element maker
 
-def getTimeInstant( time , id = None ) : 
-    return E.TimeInstant( getId(id), E.timePosition( str(time) ) )
+E = ElementMaker(namespace=NS,nsmap=NS_MAP)
 
-def getTimePeriod( begin , end , id = None ) : 
-    return E.TimePeriod( getId(id), E.beginPosition( str(begin) ) ,
-                                    E.endPosition( str(end) ) ) 
+#------------------------------------------------------------------------------
+# predefined fully qualified names
 
-def getTime( begin, end = None , id = None ): 
-    if end is None : 
-        return getTimeInstant( begin, id ) 
-    else : 
-        return getTimePeriod( begin , end , id )
+# attributes
+id = nn(NS,"id")
+ID = id
+srsName = nn(None,"srsName")
+srsName = nn(None,"srsDimension")
+axisLabels = nn(None,"axisLabels")
+uomLabels = nn(None,"uomLabels")
+
+# elements
+TimeInstant = nn(NS,"TimeInstant")
+TimePeriod = nn(NS,"TimePeriod")
+MultiSurface = nn(NS,"MultiSurface")
+Polygon = nn(NS,"Polygon")
+LinearRing = nn(NS,"LinearRing")
+
+beginPosition = nn(NS,"beginPosition")
+endPosition = nn(NS,"endPosition")
+timePosition = nn(NS,"timePosition")
+surfaceMember = nn(NS,"surfaceMember")
+exterior = nn(NS,"exterior")
+interior = nn(NS,"interior")
+posList = nn(NS,"posList")
+
+#X=nn(NS,"X")
+
+#------------------------------------------------------------------------------
+# element factories
+
+def getTimeInstant(time, id=None):
+    return E.TimeInstant(getId(id), E.timePosition(str(time)))
+
+def getTimePeriod(begin, end, id=None):
+    return E.TimePeriod(getId(id), E.beginPosition(str(begin)),
+                                    E.endPosition(str(end)))
+
+def getTime(begin, end=None, id=None):
+    if end is None:
+        return getTimeInstant(begin, id)
+    else:
+        return getTimePeriod(begin, end, id)
 
 #------------------------------------------------------------------------------
 
-def setSRSName( e , srs_name ): 
-    if srs_name : e.set("srsName",str(srs_name))
-    return e 
+def setSRSName(e, srs_name):
+    if srs_name: e.set("srsName",str(srs_name))
+    return e
 
-def setSRSDimension( e , srs_dim ): 
-    srs_dim = int( srs_dim ) 
-    if ( srs_dim > 0 ) : e.set("srsDimension",str(srs_dim))
-    return e 
+def setSRSDimension(e, srs_dim):
+    srs_dim = int(srs_dim)
+    if (srs_dim > 0): e.set("srsDimension",str(srs_dim))
+    return e
 
 
-def setSRS( e , srs_name , srs_dim = None ) : 
-    
-    return setSRSDimension( setSRSName( e , srs_name ) , srs_dim ) 
+def setSRS(e, srs_name, srs_dim=None):
 
-def getPointList2D( geom , sr = None ) : 
+    return setSRSDimension(setSRSName(e, srs_name), srs_dim)
 
-    if sr is None : 
-        sr = geom.GetSpatialReference() 
-    
-    # check the spatial reference, adjust format and swap coordinates if needed 
-
-    if sr is None : 
-        sr = geom.GetSpatialReference() 
-
-    # check if geographic of projected 
-    if ( sr is not None ) and ( sr.IsProjected() ): 
+def _get_formater(geom, sr):
+    if sr is None:
+        sr = geom.GetSpatialReference()
+    # check if geographic of projected
+    if (sr is not None) and (sr.IsProjected()):
         # setting precision to meters
         _frm = "%.2f %.2f"
-    else : 
+    else:
         # setting precision to degrees
         _frm = "%.8f %.8f"
+    # check whether swap coordinates or not
+    _cswap = (sr is not None) and (sr.GetAuthorityName(None) == "EPSG") \
+                and (int(sr.GetAuthorityCode(None)) in EPSG_AXES_REVERSED)
+    if _cswap:
+        return lambda p: _frm%(p[1], p[0])
+    else:
+        return lambda p: _frm%(p[0], p[1])
 
-    # check whether swap coordinates or not 
-    _cswap = ( sr is not None ) and ( sr.GetAuthorityName(None) == "EPSG" ) \
-                and ( int( sr.GetAuthorityCode(None) ) in EPSG_AXES_REVERSED ) 
+def getPoint(geom, sr=None):
+    _str = _get_formater(geom, sr)
+    return E.Point(getId(), _str(geom.GetPoint(0)))
 
-    if _cswap : 
-        _str = lambda p: _frm%( p[1], p[0] ) 
-    else : 
-        _str = lambda p: _frm%( p[0], p[1] ) 
+def getPointList2D(geom, sr=None):
+    _str = _get_formater(geom, sr)
+    pl = (_str(geom.GetPoint(i)) for i in xrange(geom.GetPointCount()))
+    return E.posList(" ".join(pl))
 
-    pl = " ".join(_str(geom.GetPoint(i)) for i in xrange(geom.GetPointCount())) 
+def getLinearRing(geom, sr=None):
+    if sr is None:
+        sr = geom.GetSpatialReference()
+    if geom.GetGeometryName() != "LINEARRING":
+        raise ValueError("Invalid input: %s" % geom)
+    return E.LinearRing(getPointList2D(geom, sr))
 
-    return E.posList(pl) 
-
-
-def getLinearRing( geom , sr = None ) : 
-
-    if sr is None : 
-        sr = geom.GetSpatialReference() 
-
-    if geom.GetGeometryName() != "LINEARRING" : 
-        raise ValueError( "Invalid input: %s" % geom ) 
-
-    return E.LinearRing( getPointList2D( geom , sr ) )
-
-
-def getPolygon( geom , sr = None ) : 
-
-    if sr is None : 
-        sr = geom.GetSpatialReference() 
-
-    if geom.GetGeometryName() != "POLYGON" : 
-        raise ValueError( "Invalid input: %s" % geom ) 
-        
-    l = [ getId() ] 
-    cnt = 0 
-    for i in xrange(geom.GetGeometryCount()) :  
-        g = geom.GetGeometryRef(i) 
-        if g.GetGeometryName() == "LINEARRING" :
-            e =  E.exterior if cnt == 0 else E.interior 
-            l.append( e( getLinearRing(g,sr) ) ) 
-            cnt += 1 
-
-    return E.Polygon( *l )
+def getPolygon(geom, sr=None):
+    if sr is None:
+        sr = geom.GetSpatialReference()
+    if geom.GetGeometryName() != "POLYGON":
+        raise ValueError("Invalid input: %s" % geom)
+    l = [getId()]
+    cnt = 0
+    for i in xrange(geom.GetGeometryCount()):
+        g = geom.GetGeometryRef(i)
+        if g.GetGeometryName() == "LINEARRING":
+            e =  E.exterior if cnt == 0 else E.interior
+            l.append(e(getLinearRing(g,sr)))
+            cnt += 1
+    return E.Polygon(*l)
 
 
-def getMultiSurface( geom , sr = None ) : 
-
-    if sr is None : 
-        sr = geom.GetSpatialReference() 
-
-    l = [ getId() ] 
-
-    if geom.GetGeometryName() == "POLYGON" : 
-
-        l.append( E.surfaceMember( getPolygon(geom,sr) ) ) 
-
-    elif geom.GetGeometryName() == "MULTIPOLYGON" : 
-
-        for i in xrange(geom.GetGeometryCount()) :  
-            g = geom.GetGeometryRef(i) 
-            if g.GetGeometryName() == "POLYGON" :
-                l.append( E.surfaceMember( getPolygon(g,sr) ) ) 
+def getMultiSurface(geom, sr=None):
+    if sr is None:
+        sr = geom.GetSpatialReference()
+    l = [getId()]
+    if geom.GetGeometryName() == "POLYGON":
+        l.append(E.surfaceMember(getPolygon(geom,sr)))
+    elif geom.GetGeometryName() == "MULTIPOLYGON":
+        for i in xrange(geom.GetGeometryCount()):
+            g = geom.GetGeometryRef(i)
+            if g.GetGeometryName() == "POLYGON":
+                l.append(E.surfaceMember(getPolygon(g,sr)))
 
     # geometry collection is meant to represent empty geometry
     # non-empty collections are not supported
-    elif geom.GetGeometryName() == "GEOMETRYCOLLECTION" :
-        if 0 != geom.GetGeometryCount() :
-            raise ValueError( "Invalid input: %s" % geom )
+    elif geom.GetGeometryName() == "GEOMETRYCOLLECTION":
+        if 0 != geom.GetGeometryCount():
+            raise ValueError("Invalid input: %s" % geom)
         pass
 
     else:
-        raise ValueError( "Invalid input: %s" % geom )
+        raise ValueError("Invalid input: %s" % geom)
 
-    return E.MultiSurface( *l )
+    return E.MultiSurface(*l)
 
 
-def getSurface( geom ) : 
-
-    if geom.GetGeometryName() == "POLYGON" : 
-        return getPolygon( geom )  
-
-    elif geom.GetGeometryName() == "MULTIPOLYGON" : 
-        return getMultiSurface( geom )  
-
+def getSurface(geom):
+    if geom.GetGeometryName() == "POLYGON":
+        return getPolygon(geom)
+    elif geom.GetGeometryName() == "MULTIPOLYGON":
+        return getMultiSurface(geom)
     # geometry collection is meant to represent empty geometry
     # non-empty collections are not supported
-    elif geom.GetGeometryName() == "GEOMETRYCOLLECTION" :
-        return getMultiSurface( geom )
-    
-    else :
-        raise ValueError( "Invalid input: %s" % geom ) 
+    elif geom.GetGeometryName() == "GEOMETRYCOLLECTION":
+        return getMultiSurface(geom)
+    else:
+        raise ValueError("Invalid input: %s" % geom)
