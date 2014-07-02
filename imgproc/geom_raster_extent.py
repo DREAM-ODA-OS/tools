3e0a132d6ea8a54cc5cab46e9f12fbd206136657
martin.paces <martin.paces@eox.at>
2014-07-02 15:59:58 +0200

    mapToWGS84 moved to img_geom + style improvement

--- imgproc/geom_raster_extent.py
+++ imgproc/geom_raster_extent.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 #------------------------------------------------------------------------------
-# 
+#
 #   Extract extent of a raster image as a rectange polygon.
 #
-# Project: Image Processing Tools 
+# Project: Image Processing Tools
 # Authors: Martin Paces <martin.paces@eox.at>
 #
 #-------------------------------------------------------------------------------
@@ -12,8 +12,8 @@
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all
@@ -28,86 +28,74 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
-import sys 
-import os.path 
-import img_block as ib 
-import img_geom as ig 
-#import numpy as np 
-#import math as m 
-from osgeo import ogr ; ogr.UseExceptions() 
-from osgeo import osr ; ogr.UseExceptions() 
-from osgeo import gdal ; gdal.UseExceptions() 
+import sys
+import os.path
+import img_block as ib
+import img_geom as ig
+#import numpy as np
+#import math as m
+from osgeo import ogr ; ogr.UseExceptions()
+from osgeo import osr ; ogr.UseExceptions()
+from osgeo import gdal ; gdal.UseExceptions()
 
-#------------------------------------------------------------------------------
-
-if __name__ == "__main__" : 
-
-    # TODO: to improve CLI 
-
-    EXENAME = os.path.basename( sys.argv[0] ) 
-
-    DEBUG=False 
+if __name__ == "__main__":
+    # TODO: to improve CLI
+    EXENAME = os.path.basename(sys.argv[0])
+    DEBUG=False
     FORMAT="WKB"
-    AND = False 
+    AND = False
 
-    try: 
-
+    try:
         INPUT = sys.argv[1]
-        NP = 1 
-        if len(sys.argv) > NP : 
-            for arg in sys.argv[NP:] : 
-                if ( arg in ig.OUTPUT_FORMATS ) : FORMAT=arg # output format
-                elif ( arg == "DEBUG" ) : DEBUG = True # dump debuging output
+        NP = 1
+        if len(sys.argv) > NP:
+            for arg in sys.argv[NP:]:
+                if (arg in ig.OUTPUT_FORMATS): FORMAT=arg # output format
+                elif (arg == "DEBUG"): DEBUG = True # dump debuging output
 
-    except IndexError : 
-        
-        sys.stderr.write("ERROR: Not enough input arguments!\n") 
-        sys.stderr.write("\nExtract image extent as a rectange polygon.\n") 
-        sys.stderr.write("The unsipmified geometry is dumped to stdout,\n") 
-        sys.stderr.write("by default in WKB format.\n\n") 
-        sys.stderr.write("USAGE: %s <input image> [DEBUG]\n"%EXENAME) 
-        sys.exit(1) 
+    except IndexError:
+        print >>sys.stderr, "ERROR: Not enough input arguments!"
+        print >>sys.stderr, "\nExtract image extent as a rectange polygon."
+        print >>sys.stderr, "The unsipmified geometry is dumped to stdout,"
+        print >>sys.stderr, "by default in WKB format."
+        print >>sys.stderr
+        print >>sys.stderr, "USAGE: %s <input image> [DEBUG]"%EXENAME
+        sys.exit(1)
 
     #--------------------------------------------------------------------------
 
-    # open input image 
-    imi = ib.ImgFileIn( INPUT ) 
+    # open input image
+    imi = ib.ImgFileIn(INPUT)
 
     # has geocoding?
-    if imi.ds.GetProjection() : 
+    if imi.ds.GetProjection():
         # get the transformation matrix
-        x0 , dxx , dxy , y0 , dyx , dyy = imi.ds.GetGeoTransform()
-        sr = osr.SpatialReference( imi.ds.GetProjection() ) 
-        #print "PROJECTION: " , imi.ds.GetProjection()
-    else : 
-        # operate in image coordinate space 
-        x0 , dxx , dxy , y0 , dyx , dyy = 0.0 , 1.0 , 0.0 , 0.0 , 0.0 , 1.0  
-        sr = None 
+        x0, dxx, dxy, y0, dyx, dyy = imi.ds.GetGeoTransform()
+        sr = osr.SpatialReference(imi.ds.GetProjection())
+        #print "PROJECTION: ", imi.ds.GetProjection()
+    else:
+        # operate in image coordinate space
+        x0, dxx, dxy, y0, dyx, dyy = 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
+        sr = None
 
-    # calculate the corner coordinates 
-
-    r = ogr.Geometry( ogr.wkbLinearRing ) 
-    
-    for x,y in ((0,0),(imi.sx,0),(imi.sx,imi.sy),(0,imi.sy)) :
-        r.AddPoint_2D( x0+dxx*x+dxy*y , y0+dyx*x+dyy*y ) 
-
+    # calculate the corner coordinates
+    r = ogr.Geometry(ogr.wkbLinearRing)
+    for x, y in ((0, 0), (imi.sx, 0), (imi.sx, imi.sy), (0, imi.sy)):
+        r.AddPoint_2D(x0+dxx*x+dxy*y, y0+dyx*x+dyy*y)
     r.CloseRings()
 
-    # create polygon 
+    # create polygon
+    geom = ogr.Geometry(ogr.wkbPolygon)
+    geom.AddGeometry(r)
 
-    geom = ogr.Geometry( ogr.wkbPolygon ) 
-    geom.AddGeometry( r ) 
-
-    # assign spatial reference 
-    geom.AssignSpatialReference( sr )  
+    # assign spatial reference
+    geom.AssignSpatialReference(sr)
 
     #--------------------------------------------------------------------------
-    # export 
+    # export
 
-    try: 
-
-        sys.stdout.write(ig.dumpGeom(geom,FORMAT)) 
-
-    except Exception as e : 
-        print >>sys.stderr, "ERROR: %s: %s"%(EXENAME,e)
+    try:
+        sys.stdout.write(ig.dumpGeom(geom, FORMAT))
+    except Exception as e:
+        print >>sys.stderr, "ERROR: %s: %s"%(EXENAME, e)
         sys.exit(1)
--- imgproc/img_geom.py
+++ imgproc/img_geom.py
@@ -210,9 +210,138 @@ def wrapArroundWGS84(geom, nstep=200):
         nstep controls the split border segmentation (dy = (ymax-ymin)/nstep)
 
         eqivalent to:
-            wrapArroundDateLine(geom,(-180.,-90.,+180.,+90.), nstep)
+            wrapArroundDateLine(geom, (-180., -90., +180., +90.), nstep)
     """
     return wrapArroundDateLine(geom, (-180., -90., +180., +90.), nstep)
+
+#-------------------------------------------------------------------------------
+
+def mapToWGS84(geom):
+
+    def between(v, (v0, v1)):
+        if v0 <= v1:
+            return (v0 <= v)and(v1 >= v)
+        else: #v1 > v0
+            return (v1 <= v)and(v0 >= v)
+
+    def extent_contains(x0, y0):
+        return ((x0_min <= x0)and(x0_max >= x0)
+             and(y0_min <= y0)and(y0_max >= y0))
+
+    def generate_polar_section(north, east):
+        eps = 1e-9
+        y00 = 89 # max. opposite pole lat.distnace from the equator
+        x0 = 0 if east else -180
+        y0 = (-y00) if north else (+y00)
+        y1 = (90-eps) if north else (eps-90)
+
+        lr = ogr.Geometry(type=ogr.wkbLinearRing)
+        for i in xrange(31):
+            lr.AddPoint_2D(i*6+x0, y0)
+        lr.AddPoint_2D(180+x0, y1)
+        lr.AddPoint_2D(x0, y1)
+        lr.AddPoint_2D(x0, y0)
+        p = ogr.Geometry(type=ogr.wkbPolygon)
+        p.AddGeometry(lr)
+        p.AssignSpatialReference(OSR_WGS84)
+        return p
+
+    def fix_dateline(geom, east):
+        """fix the +/-180dg ambiguity of the date-line nodes"""
+        def _dlflip_east((x, y, _)): # date-line point flipper
+            return (x+360.0 if x < -179.0 else x, y)
+        def _dlflip_west((x, y, _)): # date-line point flipper
+            return (x-360.0 if x > (+179.0) else x, y)
+        return Transfomer(_dlflip_east if east else _dlflip_west)(geom)
+
+    def transform_polar(north):
+        # generate polygon spliting the polar geometry to halves
+        s1 = generate_polar_section(north, east=True)
+        s2 = generate_polar_section(north, east=False)
+
+        # transform coordinates
+        s1.Transform(ct_rev)
+        s2.Transform(ct_rev)
+
+        # split the polar geometry to halves
+        g1 = geom.Intersection(s1)
+        g2 = geom.Intersection(s2)
+
+        # transform halves to the target projection
+        g1.Transform(ct_fwd)
+        g2.Transform(ct_fwd)
+
+        # fix the dateline ambiguity
+        g1 = fix_dateline(g1, east=True)
+        g2 = fix_dateline(g2, east=False)
+
+        # return the unified geometry
+        return g1.Union(g2)
+
+    #--------------------------------------------------------------------------
+
+    sr_src = geom.GetSpatialReference()
+    sr_dst = OSR_WGS84
+
+    # coordinate transformation objects
+    ct_fwd = osr.CoordinateTransformation(sr_src, sr_dst)
+    ct_rev = osr.CoordinateTransformation(sr_dst, sr_src)
+
+    # envelope and centroid in the source coordinates
+    x0_min, x0_max, y0_min, y0_max = geom.GetEnvelope()
+
+    # centroid
+    x0_cnt, y0_cnt = 0.5*(x0_min+x0_max), 0.5*(y0_min+y0_max)
+
+    # try to get coordinates of the north and south pole in the source CRS
+    try:
+        xy0_np = ct_rev.TransformPoint(0.0, 90.0)[:2]
+    except RuntimeError:
+        xy0_np = None
+
+    try:
+        xy0_sp = ct_rev.TransformPoint(0.0, -90.0)[:2]
+    except RuntimeError:
+        xy0_sp = None
+
+    # case #1 - extent contains the north pole
+    if xy0_np and extent_contains(*xy0_np):
+        return setSR(transform_polar(north=True), OSR_WGS84)
+
+    # case #2 - extent contains the south pole
+    # check whether the extent contains the south pole
+    elif xy0_sp and extent_contains(*xy0_sp):
+        return setSR(transform_polar(north=False), OSR_WGS84)
+
+    # case #3 proceed with the date-line handling
+
+    # perform transformation
+    geom.Transform(ct_fwd)
+
+    # get extent and centroid in the target coordinates
+    x1_min, _, _ = ct_fwd.TransformPoint(x0_min, y0_cnt)
+    x1_max, _, _ = ct_fwd.TransformPoint(x0_max, y0_cnt)
+    x1_cnt, _, _ = ct_fwd.TransformPoint(x0_cnt, y0_cnt)
+
+    # fix the wild easting wrap-arround
+    if not between(x1_cnt, (x1_min, x1_max)):
+        if x1_max < x1_min: # axis orientation preserved
+            x_cnt, x_min, x_max = x1_cnt, x1_min, x1_max
+        else: # (x1_min < x1_max) # flipped axis orientation
+            x_cnt, x_min, x_max = x1_cnt, x1_max, x1_min
+
+        # point unwrapping fuctions
+        if x_cnt < x_max: # EAST to WEST
+            def _dlflip(p):
+                return (p[0]-360*(p[0] > x_max), p[1])
+        elif x_cnt > x_min: # WEST to EAST
+            def _dlflip(p):
+                return (p[0]+360*(p[0] < x_min), p[1])
+
+        geom = setSR(Transfomer(_dlflip)(geom), OSR_WGS84)
+
+    # perform proper wrapparround
+    return setSR(wrapArroundDateLine(geom, (-180, -90, 180, 90), 1), OSR_WGS84)
 
 #-------------------------------------------------------------------------------
 
@@ -322,7 +451,7 @@ class Transfomer(object):
         for i in xrange(r0.GetPointCount()):
             rv = (self.__f)(r0.GetPoint(i), *prm, **kw)
             if rv is not None:
-                r1.AddPoint(*rv)
+                r1.AddPoint_2D(*rv)
         return r1
 
     def _polygon(self, p0, *prm, **kw):
