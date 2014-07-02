7eb6028e1c22d1aff4e22d38ced25ba82dac3f5f
martin.paces <martin.paces@eox.at>
2014-07-02 19:16:40 +0200

    Adding missing NS definitions + style improvement.

--- metadata/ns_opt20.py
+++ metadata/ns_opt20.py
@@ -1,8 +1,8 @@
 #------------------------------------------------------------------------------
-# 
+#
 #   EOP-OPT v1.0 namespace
 #
-# Project: XML Metadata Handling 
+# Project: XML Metadata Handling
 # Authors: Martin Paces <martin.paces@eox.at>
 #
 #-------------------------------------------------------------------------------
@@ -11,8 +11,8 @@
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all
@@ -27,33 +27,53 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
-from lxml import etree 
 from lxml.builder import ElementMaker
-from xml_utils import nn 
+from xml_utils import nn
+
+import ns_eop20 as ns_eop
+import ns_om20 as ns_om
+import ns_ows20 as ns_ows
+import ns_gml32 as ns_gml
+import ns_swe10 as ns_swe
+import ns_xsi as ns_xsi
+import ns_xlink as ns_xlink
 
 #------------------------------------------------------------------------------
-# namespace 
+# namespace
+#------------------------------------------------------------------------------
+# schema location attribute
 
-NS="http://www.opengis.net/opt/2.0"
-
-NS_MAP={ "opt" : NS } 
+# SchemaTron Rules
+STR = "http://schemas.opengis.net/omeo/1.0/schematron_rules_for_eop.xsl"
+# schema
+SL = "http://www.opengis.net/opt/2.0 http://schemas.opengis.net/omeo/1.0/opt.xsd"
 
 #------------------------------------------------------------------------------
-# element maker 
-
-E = ElementMaker(namespace=NS,nsmap=NS_MAP) 
+# namespace
+NS = "http://www.opengis.net/opt/2.0"
+NS_MAP = {"opt": NS,
+          "eop": ns_eop.NS,
+          "om": ns_om.NS,
+          "ows": ns_ows.NS,
+          "gml": ns_gml.NS,
+          "swe": ns_swe.NS,
+          "xsi": ns_xsi.NS,
+          "xlink": ns_xlink.NS}
 
 #------------------------------------------------------------------------------
-# predefined fully qualified names 
+# element maker
+E = ElementMaker(namespace=NS, nsmap=NS_MAP)
 
-# attributes 
+#------------------------------------------------------------------------------
+# predefined fully qualified names
 
-# elements 
+# attributes
 
-EarthObservationResult=nn(NS,"EarthObservationResult") 
-cloudCoverPercentage=nn(NS,"cloudCoverPercentage") 
-cloudCoverPercentageQuotationMode=nn(NS,"cloudCoverPercentageQuotationMode") 
-snowCoverPercentage=nn(NS,"snowCoverPercentage") 
-snowCoverPercentageQuotationMode=nn(NS,"snowCoverPercentageQuotationMode") 
+# elements
+EarthObservationResult = nn(NS, "EarthObservationResult")
+cloudCoverPercentage = nn(NS, "cloudCoverPercentage")
+cloudCoverPercentageQuotationMode = nn(NS, "cloudCoverPercentageQuotationMode")
+snowCoverPercentage = nn(NS, "snowCoverPercentage")
+snowCoverPercentageQuotationMode = nn(NS, "snowCoverPercentageQuotationMode")
 
-#X=nn(NS,"X") 
+#X = nn(NS, "X")
