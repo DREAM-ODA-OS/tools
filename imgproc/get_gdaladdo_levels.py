8c2c535df8f26705f2cc221e9b326b0ab09af3ac
martin.paces <martin.paces@eox.at>
2014-07-10 23:59:29 +0200

    Code clean-up.

--- imgproc/get_gdaladdo_levels.py
+++ imgproc/get_gdaladdo_levels.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 #------------------------------------------------------------------------------
-# 
-#   calculate possible >gdaladdo< overview levels 
 #
-#   Optionally the stretching can be performed in logarithmic (dB) scale. 
+#   calculate possible >gdaladdo< overview levels
 #
-# Project: Image Processing Tools 
+#   Optionally the stretching can be performed in logarithmic (dB) scale.
+#
+# Project: Image Processing Tools
 # Authors: Martin Paces <martin.paces@eox.at>
 #
 #-------------------------------------------------------------------------------
@@ -14,8 +14,8 @@
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all
@@ -30,50 +30,37 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
-import sys 
-import os.path 
-import img_block as ib 
-import numpy as np 
+import sys
+import os.path
+import img_block as ib
+import numpy as np
 
-#------------------------------------------------------------------------------
+if __name__ == "__main__":
+    # TODO: to improve CLI
+    exename = os.path.basename(sys.argv[0])
+    # block size
+    bsx, bsy = 256, 256
+    dbscale = False
 
-if __name__ == "__main__" : 
-
-    # TODO: to improve CLI 
-
-    exename = os.path.basename( sys.argv[0] ) 
-    # block size 
-    bsx , bsy = 256, 256 
-    dbscale = False ; 
-
-    # default format options 
-
-    try: 
-
-        INPUT  = sys.argv[1]
+    try:
+        INPUT = sys.argv[1]
         STOP_MAX = int(sys.argv[2])
-        STOP_MIN = int(sys.argv[3]) 
+        STOP_MIN = int(sys.argv[3])
 
-    except IndexError : 
-        
-        sys.stderr.write("Not enough input arguments!\n") 
-        sys.stderr.write("USAGE: %s <input image> <stop_size_max> <stop_size_min>\n"%exename) 
-        sys.stderr.write("EXAMPLE: %s input.tif 256 16\n"%exename) 
-        sys.exit(1) 
+    except IndexError:
+        sys.stderr.write("Not enough input arguments!\n")
+        sys.stderr.write("USAGE: %s <input image> <stop_size_max> <stop_size_min>\n"%exename)
+        sys.stderr.write("EXAMPLE: %s input.tif 256 16\n"%exename)
+        sys.exit(1)
 
-    # open input image 
-    imi = ib.ImgFileIn( INPUT ) 
+    level = []
+    factor = 1
+    imi = ib.ImgFileIn(INPUT)
+    size = np.array([imi.sy, imi.sx], "int32")
 
-#------------------------------------------------------------------------------
+    while np.all(size >= STOP_MIN) and np.any(size > STOP_MAX):
+        factor *= 2
+        level.append(factor)
+        size = (size + 1) / 2
 
-    level  = []
-    factor = 1 
-    size   = np.array( [imi.sy, imi.sx] , "int32" ) 
-
-    while np.all(size >= STOP_MIN) and np.any(size > STOP_MAX) :
-
-        factor *= 2 
-        level.append( factor ) 
-        size  = ( size + 1 ) / 2 
-
-    print " ".join(map(lambda v : "%d"%v , level))
+    print " ".join("%d"%v for v in level)
